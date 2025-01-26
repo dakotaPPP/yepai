f6acd7558302ac68c082b3bc858ddfd8b75cf714
dakotaPPP <dakota.pound04@gmail.com>
2025-01-26 01:22:27 -0600

    clensed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Card } from "@/components/ui/card"
 import { cars, questions } from "../data/cars"
 import type { Car, Message, Question } from "../types/cars"
 import { CarCard } from "../components/car-card"
-import { ChatInterface } from "../components/chat-interface"
 import { motion, AnimatePresence} from "framer-motion"
 import { ChatWithSuggestions } from "@/components/chat-box/chat-with-suggestions"
 
